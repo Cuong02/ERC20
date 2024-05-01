d6558d3a49cb7ef7503baf4335b9c25a31820d47
Quang Cường <thanlequangcuong@gmail.com>
2024-05-02 01:48:23 +0700

    Test and deploy contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,12 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-toolbox")
+require('dotenv').config()
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    bsctest: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      accounts: [process.env.PRIV_KEY],
+    }
+  }
 };
